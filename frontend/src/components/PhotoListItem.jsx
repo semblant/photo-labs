f50479b2de5dad95476c2deff9ef078581ea5878
semblant <robynhumphreys7_4@hotmail.com>
2025-02-04 22:03:15 -0700

    Add syling to PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,13 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ photo }) => {
   const { id, location, imageSource, username, profile } = photo;
   return (
-      <div>
-        <img className="imageSource" src={imageSource}/><br/>
-        <img className="profilePicture" src={profile}/>
-        <p>{username}</p>
-        <p>{location.city}, {location.country}</p>
+      <div className="photo-list__item">
+        <img className="photo-list__image" src={imageSource}/><br/>
+          <img className="photo-list__user-profile" src={profile}/>
+          <div className="photo-list__user-details">
+            <span className="photo-list__user-info">{username}</span><br/>
+            <span className="photo-list__user-location">{location.city}, {location.country}</span>
+          </div>
       </div>
     )
 };
