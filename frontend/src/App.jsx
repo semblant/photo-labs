dcd96891c6e819279b243b014b323a8ad89af908
semblant <robynhumphreys7_4@hotmail.com>
2025-02-04 21:17:18 -0700

    Add PhotoListItem component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,25 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const { id, location, imageSource, username, profile } = sampleDataForPhotoListItem;
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem id={id} location={location} imageSource={imageSource} username={username} profile={profile}/>
     </div>
   );
 };
