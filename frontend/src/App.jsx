3da74dc1d76cfed86a75b1096dde41b8e2bb5f18
semblant <robynhumphreys7_4@hotmail.com>
2025-02-08 21:40:56 -0700

    Implement the HomeRoute component, change App component to call HomeRoute only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,14 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import PhotoFavButton from 'components/PhotoFavButton';
-import PhotoList from 'components/PhotoList';
-import TopicListItem from 'components/TopicListItem';
-import TopicList from 'components/TopicList';
-import TopNavigationBar from 'components/TopNavigationBar'
-
-
-// create an array of photos -- using .map(_,index) to have unique keys for each photo (since they're all the same photo). Once there are new id's change to the below
-/* const photos = [...Array(3)].map((_, index) => {
-<PhotoListItem key={sampleDataForPhotoListItem.id} photo={sampleDataForPhotoListItem}/>})
-*/
-
-/*
-commenting out to upgrade
-
-
-const photos = [...Array(3)].map((_, index) =>
-  <PhotoListItem key={index} photo={sampleDataForPhotoListItem}/>
-);
-*/
-
+import HomeRoute from 'routes/HomeRoute';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <nav>
-        <TopNavigationBar />
-      </nav>
-      <div className='photo-list'>
-        {/*photos*/}
-        <PhotoList />
-      </div>
+      <HomeRoute />
     </div>
   );
 };
