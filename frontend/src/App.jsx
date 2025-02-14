038eae560f61a71b7e67ebab5584680e1d84dc29
semblant <robynhumphreys7_4@hotmail.com>
2025-02-13 20:44:15 -0700

    Add TopNavigationBar component funcitonality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,26 +10,18 @@ import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  /*
-  Currently, your implementation only allows for one photo to be marked as a favourite at a time. The favourite state is a boolean, which means it can only hold the favourite status of one photo.
-  If you want to allow users to favourite multiple photos, you might need to change the favourite state to an array that can hold the IDs of multiple photos.
-  Then, in your switchFavourite function, you would check if a photo's ID is already in the favourite array.
-  If it is, you would remove it from the array, and if it's not, you would add it to the array.
-
-
-  BUG: clicking one favourite, favourites everything at the same time. -- access via keys?
-
-  Need to:
-  Put notification on favourites in nav bar
-  */
+  /* Favourites */
   const [favourite, setFavourite] = useState({}); // store current state of favourite as an empty object
   const switchFavourite = (photoId) => {
-    setFavourite((prevFavourites) => ({
-      ...prevFavourites,
-      [photoId]: !prevFavourites[photoId]
-    }));
-  }
+    setFavourite((prevFavourites) => {
+     const newFavourites = {...prevFavourites};
 
+    if (newFavourites[photoId]) delete newFavourites[photoId] // Remove current key if 'unfavourited'
+    else newFavourites[photoId] = true;
+
+    return newFavourites;
+    });
+  }
 
 
 
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,14 +4,16 @@ import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 
-const TopNavigation = ({ topics, favourite }) => {
+const TopNavigationBar = ({ topics, favourite }) => {
+  const hasFavourites = Object.keys(favourite).some((status) => status); // Check if at least one favourite in the object
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} />
-      <FavBadge isFavPhotoExist={favourite} />
+      <FavBadge isFavPhotoExist={hasFavourites}/>
     </div>
   )
 }
 
-export default TopNavigation;
+export default TopNavigationBar;
