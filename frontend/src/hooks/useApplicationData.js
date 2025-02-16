7806daf524e8beedf69ada8749d0792d7318e3b8
semblant <robynhumphreys7_4@hotmail.com>
2025-02-16 16:29:23 -0700

    Implement useReducer instead of useState within useApplicationData.js

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,27 +1,58 @@
-import { useState, useEffect } from "react";
+import { useState, useReducer } from "react";
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+};
+
+function reducer(state, action) {
+  console.log('state', state);
+  console.log('action', action)
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return {...state, [action.payload.id]: true}
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      const prevFavs = {...state};
+      delete prevFavs[action.value];
+      return prevFavs;
+    case ACTIONS.SET_PHOTO_DATA:
+      return action.value
+    case ACTIONS.SET_TOPIC_DATA:
+      return
+    case ACTIONS.SELECT_PHOTO:
+      return
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      return
+  }
+
+
+  /* 'default' doesn't work?
+  default:
+  throw new Error(
+    `Tried to reduce with unsupported action type: ${action.type}`
+  )
+  */
+};
+
 
 const useApplicationData = () => {
     /* Favourites */
-    const [favourite, setFavourite] = useState({}); // store current state of favourite as an empty object
-
+    const [favourite, favDispatch] = useReducer(reducer, {}); // store current state of favourite as an empty object
     const updateToFavPhotoIds = (photoId) => {
-      setFavourite((prevFavourites) => {
-        console.log('prevFavourites', prevFavourites)
-       const newFavourites = { ...(prevFavourites) || {} };
-
-      if (newFavourites[photoId]) delete newFavourites[photoId] // Remove current key if 'unfavourited'
-      else newFavourites[photoId] = true;
-        console.log('newFavourites', newFavourites)
-      return newFavourites;
-      });
+      if (!favourite[photoId]) favDispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: {id: photoId, isFavourite: false}});
+      else favDispatch(({ type: ACTIONS.FAV_PHOTO_REMOVED, value:photoId}))
     }
 
     /* PhotoDetailsModal Component */
-    const [modal, setModal] = useState(null); // tracks one state?
+    const [modal, modalDispatch] = useReducer(reducer, null);
 
-    const setPhotoSelected = (photo) => setModal(photo); // opens modal with photo that was clicked
+    const setPhotoSelected = (photo) => modalDispatch({type: ACTIONS.SET_PHOTO_DATA, value: photo}); // opens modal with photo that was clicked
 
-    const onClosePhotoDetailsModal = () => setModal(null); // Closes modal
+    const onClosePhotoDetailsModal = () => modalDispatch({type: ACTIONS.SET_PHOTO_DATA, value: null}); // Closes modal
 
     return {
       favourite,
